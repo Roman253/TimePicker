b13d81085268fa271befdb9820c9b927791723ef
Roman253 <romang82@gmail.com>
2021-06-22 00:55:59 +0300

    Avoid repetitions css, loops in js, changed to us media queries

--- src/Clock.js
+++ src/Clock.js
@@ -8,8 +8,9 @@ export default class Clock extends Component {
       isPickingHours: true,
       isAm: false,
       setHour: 12,
-      setMinute: '00',
-      setAmOrPm: 'am'
+      setMinute: '0',
+      setAmOrPm: 'AM',
+      buttons: [],
       };
   }
 
@@ -21,14 +22,36 @@ export default class Clock extends Component {
     }, 1000);
   }
 
-  /*componentWillMount() {
-    clearInterval(this.timerId);
-  }*/
+  digitsHours() { 
+      const buttons = [];
+      for (let i = 1; i < 13 ; ++i) {
+        buttons.push(<button className={"clock-digit" + " digit" + (i) }
+        onClick={()=>this.setState({isPickingHours: false,setHour: (i)})}>{i}</button>)
+      }
+      return buttons;
+  }
+ 
+  pad(n){
+    return (n<10) ? ("0"+n):n;
+  }
+  
+  digitsMinuts() { 
+    const buttons = [];
+    for (let i = 0, k = 5 ; i < 12 ; ++i, k+=5) {
+      if (k === 60) {k=0};
+      buttons.push(<button className={"clock-digit" + " digit" + (i+1) }
+      onClick={()=>this.setState({isPickingHours: true,setMinute: (k)})}>{k}</button>);
+    }
+    return buttons;
+  }
+
+
 
   render() {
     return (
       <div className="container">
-        <h2>Time set to {this.state.setHour}:{this.state.setMinute} {this.state.setAmOrPm}</h2>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <h2>Time set to {this.state.setAmOrPm} {this.state.setHour}:{this.pad(this.state.setMinute)} </h2>
         <div className="clock">
           <div
             className="hour_hand"
@@ -50,47 +73,17 @@ export default class Clock extends Component {
           />
 
           {
-            this.state.isPickingHours ?
-              <>
-                <button className="button12" onClick={()=>this.setState({isPickingHours: false,setHour: 12})}>12</button>
-                <button className="button1" onClick={()=>this.setState({isPickingHours: false,setHour: 1})}>1</button>
-                <button className="button2" onClick={()=>this.setState({isPickingHours: false,setHour: 2})}>2</button>
-                <button className="button3" onClick={()=>this.setState({isPickingHours: false,setHour: 3})}>3</button>
-                <button className="button4" onClick={()=>this.setState({isPickingHours: false,setHour: 4})}>4</button>
-                <button className="button5" onClick={()=>this.setState({isPickingHours: false,setHour: 5})}>5</button>
-                <button className="button6" onClick={()=>this.setState({isPickingHours: false,setHour: 6})}>6</button>
-                <button className="button7" onClick={()=>this.setState({isPickingHours: false,setHour: 7})}>7</button>
-                <button className="button8" onClick={()=>this.setState({isPickingHours: false,setHour: 8})}>8</button>
-                <button className="button9" onClick={()=>this.setState({isPickingHours: false,setHour: 9})}>9</button>
-                <button className="button10" onClick={()=>this.setState({isPickingHours: false,setHour: 10})}>10</button>
-                <button className="button11" onClick={()=>this.setState({isPickingHours: false,setHour: 11})}>11</button>
-
-              </>
-            :
-              <>
-                <button className="button12" onClick={()=>this.setState({isPickingHours: true,setMinute: '00'})}>0</button>
-                <button className="button1" onClick={()=>this.setState({isPickingHours: true,setMinute: '05'})}>5</button>
-                <button className="button2" onClick={()=>this.setState({isPickingHours: true,setMinute: '10'})}>10</button>
-                <button className="button3" onClick={()=>this.setState({isPickingHours: true,setMinute: '15'})}>15</button>
-                <button className="button4" onClick={()=>this.setState({isPickingHours: true,setMinute: '20'})}>20</button>
-                <button className="button5" onClick={()=>this.setState({isPickingHours: true,setMinute: '25'})}>25</button>
-                <button className="button6" onClick={()=>this.setState({isPickingHours: true,setMinute: '30'})}>30</button>
-                <button className="button7" onClick={()=>this.setState({isPickingHours: true,setMinute: '35'})}>35</button>
-                <button className="button8" onClick={()=>this.setState({isPickingHours: true,setMinute: '40'})}>40</button>
-                <button className="button9" onClick={()=>this.setState({isPickingHours: true,setMinute: '45'})}>45</button>
-                <button className="button10" onClick={()=>this.setState({isPickingHours: true,setMinute: '50'})}>50</button>
-                <button className="button11" onClick={()=>this.setState({isPickingHours: true,setMinute: '55'})}>55</button>
-              </>
+            this.state.isPickingHours ? <> {this.digitsHours()} </>  : <> {this.digitsMinuts()} </>
           }
           {
-                this.state.isAm ?
-                <>
-                <button className="ampm" onClick={()=>this.setState({isAm: false, setAmOrPm: 'am'})}>am</button>
-                </>
-              :
-                <>
-                <button className="ampm" onClick={()=>this.setState({isAm: true,setAmOrPm: 'pm'})}>pm</button>
-                </>
+            this.state.isAm ?
+            <>
+            <button className="ampm" onClick={()=>this.setState({isAm: false, setAmOrPm: 'AM'})}>AM</button>
+            </>
+            :
+            <>
+            <button className="ampm" onClick={()=>this.setState({isAm: true,setAmOrPm: 'PM'})}>PM</button>
+            </>
           }
           
           
